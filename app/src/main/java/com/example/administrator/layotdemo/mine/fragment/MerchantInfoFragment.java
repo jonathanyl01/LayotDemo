458c9aa90b7d5ef109d7442daa54e4b838f45449
yljonathan <17768373003@163.com>
2017-05-10 09:24:45 +0800

    完成主要界面的搭建

---
+++ app/src/main/java/com/example/administrator/layotdemo/mine/fragment/MerchantInfoFragment.java
@@ -0,0 +1,19 @@
+package com.example.administrator.layotdemo.mine.fragment;
+
+import com.example.administrator.layotdemo.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/5/10.
+ */
+
+public class MerchantInfoFragment extends BaseFragment {
+    @Override
+    protected int getContentViewLayout() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
