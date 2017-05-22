d48af42e99bb3a8fb3800b3ede8d739010c42cfc
yljonathan <17768373003@163.com>
2017-05-22 17:57:51 +0800

    二维码扫描实现

--- app/src/main/java/com/example/administrator/layotdemo/mine/MySettingActivity.java
+++ app/src/main/java/com/example/administrator/layotdemo/mine/MySettingActivity.java
@@ -2,12 +2,16 @@ package com.example.administrator.layotdemo.mine;
 
 import android.content.Intent;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import com.example.administrator.layotdemo.R;
 import com.example.administrator.layotdemo.base.BaseActivity;
+import com.example.administrator.layotdemo.mine.acyivity.MessageRemindActivity;
+import com.example.administrator.layotdemo.utils.ActivityUtils;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 public class MySettingActivity extends BaseActivity {
 
@@ -25,6 +29,7 @@ public class MySettingActivity extends BaseActivity {
     RelativeLayout reAboutlf;
     @BindView(R.id.re_clean)
     RelativeLayout reClean;
+    private ActivityUtils activityUtils;
 
     @Override
     protected void initView() {
@@ -58,4 +63,26 @@ public class MySettingActivity extends BaseActivity {
     }
 
 
+
+    @OnClick({R.id.re_changepassword, R.id.re_pravacy, R.id.re_attention, R.id.re_opinion, R.id.re_aboutlf, R.id.re_clean})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.re_changepassword:
+                break;
+            case R.id.re_pravacy:
+                break;
+            case R.id.re_attention:
+                if (activityUtils == null){
+                    activityUtils = new ActivityUtils();
+                }
+                activityUtils.openActivity(this, MessageRemindActivity.class);
+                break;
+            case R.id.re_opinion:
+                break;
+            case R.id.re_aboutlf:
+                break;
+            case R.id.re_clean:
+                break;
+        }
+    }
 }
